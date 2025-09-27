ff0f6c7d5a18aea4a2cb73c347b0efdc270f1ed8
DanGG <dan.g.g@hotmail.es>
2025-09-27 17:17:16 -0600

    Se crea logica para busqueda de usuarios en vista de UsuarioAbogado

--- Preacepta.Modelos/obj/Debug/net8.0/Preacepta.Modelos.AssemblyInfo.cs
+++ Preacepta.Modelos/obj/Debug/net8.0/Preacepta.Modelos.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Preacepta.Modelos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+77e50703b12ba6a3d6328832309063e032606024")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b4f2925aea34036ca9d45a01092be4a875c8c50a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Preacepta.Modelos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Preacepta.Modelos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Preacepta.UI/wwwroot/lyso/js/BuscarUsuario.js
@@ -0,0 +1,44 @@
+﻿document.addEventListener("click", function (event) {
+    if (event.target.classList.contains("contact-btn--primario")) {
+        const cedula = document.getElementById('InputBuscarPersona').value;
+        const id = cedula;
+        console.log("Valor seleccionado:", id);
+
+        if (id) {
+            fetch(`/Personas/DetallesPersona/${id}`)
+                .then(response => response.json())
+                .then(data => {
+                    console.log(Object.keys(data));
+                    console.log("Detalles obtenidos:", data);
+                    // Aquí puedes actualizar el contenido del modal con los datos obtenidos
+                    console.log("NombreEtapa:", data.Nombre);
+
+                    if (data.bandera == true)
+                    {
+                        document.getElementById('Nombre').innerHTML = `<p>${data.nombre}</p>`;
+                        document.getElementById('Apellido1').innerHTML = `<p>${data.apellido1} ${data.apellido2}</p>`;
+                        document.getElementById('Oficio').innerHTML = `<p>${data.ocupacion}</p>`;
+                        document.getElementById('Telefono').innerHTML = `<p>${data.telefono}</p>`;
+                        document.getElementById('Correo').innerHTML = `<p>${data.correo}</p>`;
+                    }
+
+                    if (data.bandera == false)
+                    {
+                        document.getElementById('Nombre').innerHTML = `<p>No encontrado</p>`;
+                        document.getElementById('Apellido1').innerHTML = `<p>No encontrado</p>`;
+                        document.getElementById('Oficio').innerHTML = `<p>No encontrado</p>`;
+                        document.getElementById('Telefono').innerHTML = `<p>No encontrado</p>`;
+                        document.getElementById('Correo').innerHTML = `<p>No encontrado</p>`;
+
+                    }
+
+
+
+                    
+                })
+                .catch(error => {
+                    console.error("Error al obtener detalles:", error);
+                });
+        }
+    }
+});
